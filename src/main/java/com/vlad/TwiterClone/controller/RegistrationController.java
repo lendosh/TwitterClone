e2c0a912b542d27c194e6afd86d64bb2640179ac
lendosh <vladyslav.pomazanov@gmail.com>
2018-09-07 18:31:32 +0300

    reCaptch, rest client, rememberMe

--- src/main/java/com/vlad/TwiterClone/controller/RegistrationController.java
+++ src/main/java/com/vlad/TwiterClone/controller/RegistrationController.java
@@ -1,22 +1,37 @@
 package com.vlad.TwiterClone.controller;
 
 import com.vlad.TwiterClone.domain.User;
+import com.vlad.TwiterClone.domain.dto.CaptchaResponseDto;
 import com.vlad.TwiterClone.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import org.thymeleaf.util.StringUtils;
 
 import javax.validation.Valid;
+import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
+
+    private final static String CAPTCHA_URL= "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s";
+
     @Autowired
     private UserService userService;
+
+    @Value("${recaptcha.secret}")
+    private String secret;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/registration")
     public String registration(){
@@ -24,14 +39,30 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(@Valid User user,
-                          BindingResult bindingResult,
-                          Model model){
-        if(user.getPassword() != null && !user.getPassword().equals(user.getPassword2())){
+    public String addUser(
+                            @RequestParam("password2") String passwordConfirm,
+                            @RequestParam("g-recaptcha-response") String captchaResponse,
+                            @Valid User user,
+                            BindingResult bindingResult,
+                            Model model){
+
+        String url = String.format(CAPTCHA_URL, secret, captchaResponse);
+        CaptchaResponseDto response = restTemplate.postForObject(url, Collections.emptyList(), CaptchaResponseDto.class);
+
+        if(!response.isSuccess()){
+            model.addAttribute("captchaError","Fill Captcha");
+        }
+
+        boolean isConfirmEmpty = StringUtils.isEmpty(passwordConfirm);
+        if (isConfirmEmpty){
+            model.addAttribute("password2Error", "Password confirmation cann't be empty");
+        }
+
+        if(user.getPassword() != null && !user.getPassword().equals(passwordConfirm)){
             model.addAttribute("passwordError", "Password are different!");
         }
 
-        if(bindingResult.hasErrors()){
+        if(isConfirmEmpty || bindingResult.hasErrors() || !response.isSuccess()){
             Map<String, String> errors = ControllerUtils.getErrors(bindingResult);
 
             model.mergeAttributes(errors);
@@ -53,8 +84,10 @@ public class RegistrationController {
         boolean isActivated = userService.activateUser(code);
 
         if(isActivated){
+            model.addAttribute("messageType", "success");
             model.addAttribute("message", "User successfully activated");
         }else{
+            model.addAttribute("messageType", "danger");
             model.addAttribute("message", "Activation code is not found");
         }
 
--- src/main/java/com/vlad/TwiterClone/domain/User.java
+++ src/main/java/com/vlad/TwiterClone/domain/User.java
@@ -21,9 +21,6 @@ public class User implements UserDetails {
     private String username;
     @NotBlank(message = "Password cann't be empty")
     private String password;
-    @Transient
-    @NotBlank(message = "Password confirmation cann't be empty")
-    private String password2;
     private boolean active;
 
     @Email(message = "Email is not correct!")
@@ -120,13 +117,6 @@ public class User implements UserDetails {
 
     public void setActivationCode(String activationCode) {
         this.activationCode = activationCode;
-    }
-    public String getPassword2() {
-        return password2;
-    }
-
-    public void setPassword2(String password2) {
-        this.password2 = password2;
     }
 
 }
